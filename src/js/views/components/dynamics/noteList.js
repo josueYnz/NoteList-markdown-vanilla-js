21783c449fc50a2069a86c5e59d8da368a2e4469
josueYanez <josuegabriel455@gmail.com>
2020-12-31 19:36:18 -0430

    Refactoring

--- src/js/views/components/dynamics/noteList.js
+++ src/js/views/components/dynamics/noteList.js
@@ -3,15 +3,12 @@ import { noteCard } from "../containers/noteCard";
 
 export const noteList = (notes) => {
 
+  const createNoteList = document.createElement("div");
+  createNoteList.classList.add("grid-container", "gap-2");
+  createNoteList.id = "note-list";
 
   const noteListContainer = document.getElementById("note-list");
 
-  let createNoteList;
-  if (!noteListContainer) {
-    createNoteList = document.createElement("div");
-    createNoteList.classList.add("grid-container", "gap-2");
-    createNoteList.id = "note-list";
-  }
 
   const noteContent = noteListContainer ? noteListContainer : createNoteList;
 
